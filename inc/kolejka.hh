a108614377ae6fb86cb4a2fb75793bf1f2acbd4a
218682 <218682@student.pwr.edu.pl>
2016-03-21 01:11:52 +0100

    program poszerzony o liste, stos i kolejke

--- inc/kolejka.hh
+++ inc/kolejka.hh
@@ -1,14 +1,16 @@
 #ifndef KOLEJKA_HH
 #define KOLEJKA_HH
+#include "tablica.hh"
+
 class kolejka: public tablica{
-private:
-  tablica tab;
 public:
   kolejka();
-  void push(int);
   void remove(int);
-  void add(int, int);
-  void get (int);
+  void remove();
+  void add(int);
+  int get (int);
+  int first();
+  int last();
 };
 
 #endif
